ccc6687367d152d2cfe255bbca26b3413b2990e9
Diwash khatri chhetri <77787660+Diwash3@users.noreply.github.com>
2023-07-05 00:57:28 +0530

    Update railfence.cpp

--- railfence.cpp
+++ railfence.cpp
@@ -7,61 +7,61 @@ string encrypt(string text,int key)
     for(int i=0;i<key;i++)
         for(int j=0;j<text.length();j++)
             rail[i][j]='\n';
-    bool dir_down=false;
+    bool back=false;
     int row=0,col=0;
-    for(int i=0;i<text.length();i++){
+    for(int i=0;i<text.length();i++)
+    {
         if(row==0||row==key-1)
-        dir_down=!dir_down;
+            back=!back;
         rail[row][col++]=text[i];
-        dir_down?row++:row--;
-}
-string result;
-for(int i=0;i<text.length();i++)
-    for(int j=0;j<text.length();j++)
-        if (rail[i][j]!='\n')
-            result.push_back(rail[i][j]); 
-return result;
-}
-string decrypt(string cipher, int key)
-{
-    char rail[key][cipher.length()];
-    for (int i=0; i < key; i++)
-        for (int j=0; j < cipher.length(); j++)
-            rail[i][j] = '\n';
- 
-    bool dir_down;
-    int row = 0, col = 0;
-    for (int i=0; i < cipher.length(); i++)
-    {
-        if (row == 0)
-            dir_down = true;
-        if (row == key-1)
-            dir_down = false;
-        rail[row][col++] = '*';
-        dir_down?row++ : row--;
+        back?row++:row--;
     }
-    int index = 0;
-    for (int i=0; i<key; i++)
-        for (int j=0; j<cipher.length(); j++)
-            if (rail[i][j] == '*' && index<cipher.length())
-                rail[i][j] = cipher[index++];
- 
     string result;
-    row = 0, col = 0;
-    for (int i=0; i< cipher.length(); i++)
-    {
-        if (row == 0)
-            dir_down = true;
-        if (row == key-1)
-            dir_down = false;
-        if (rail[row][col] != '*')
-            result.push_back(rail[row][col++]);
-
-        dir_down?row++: row--;
-    }
+    for(int i=0;i<key;i++)
+        for(int j=0;j<text.length();j++)
+         if(rail[i][j]!='\n')
+            result.push_back(rail[i][j]);
     return result;
- 
 }
+string decrypt(string cipher,int key)
+{
+   	char rail[key][cipher.length()];
+	for (int i=0; i < key; i++)
+		for (int j=0; j < cipher.length(); j++)
+			rail[i][j] = '\n';
+
+	bool dir_down;
+	int row = 0, col = 0;
+	for (int i=0; i < cipher.length(); i++)
+	{
+		if (row == 0)
+			dir_down = true;
+		if (row == key-1)
+			dir_down = false;
+		rail[row][col++] = '*';
+		dir_down?row++ : row--;
+	}
+	int index = 0;
+	for (int i=0; i<key; i++)
+		for (int j=0; j<cipher.length(); j++)
+			if (rail[i][j] == '*' && index<cipher.length())
+				rail[i][j] = cipher[index++];
+	string result;
+	row = 0, col = 0;
+	for (int i=0; i< cipher.length(); i++)
+	{
+		if (row == 0)
+			dir_down = true;
+		if (row == key-1)
+			dir_down = false;
+		if (rail[row][col] != '*')
+			result.push_back(rail[row][col++]);
+
+		dir_down?row++: row--;
+	}
+	return result;
+}
+
 int main()
 {
     int key;
@@ -71,7 +71,7 @@ int main()
     cout<<"enter key";
     cin>>key;
     cipher=encrypt(str,key);
-    cout<<"encrypted"<<cipher<<endl;
-    cout<<"decrypted"<<decrypt(cipher,key)<<endl;
-    return 0;
+    cout<<"encrypted  " <<cipher<<endl;
+    cout<<"\ndecrypted " <<decrypt(cipher,key)<<endl;
+	return 0;
 }
